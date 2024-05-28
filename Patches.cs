b773dd07522b7442b5e8fecf0f738df1efd7311d
Oinky <contact.knilax@gmail.com>
2024-05-28 00:21:20 -0500

    Initial commit

---
+++ Patches.cs
@@ -0,0 +1,46 @@
+﻿using HarmonyLib;
+
+namespace oinky_mod;
+
+/// <summary>
+/// https://shorturl.at/ZxOqL
+/// </summary>
+internal class Patches
+{
+    /// <summary>
+    /// Visual changes.
+    /// </summary>
+    [HarmonyPatch(typeof(FMColor), "Start"), HarmonyPostfix]
+    public static void FMColorPatch(FMColor __instance)
+    {
+        /* defaults
+        film grain size = 1
+        Plugin.Logger.LogInfo($"fresnel contribution: {__instance.FresnelSettings.Contribution}"); // 0
+        Plugin.Logger.LogInfo($"fresnel scanline x: {__instance.FresnelSettings.FresnelScanlineX}"); // 0
+        Plugin.Logger.LogInfo($"fresnel scanline y: {__instance.FresnelSettings.FresnelScanlineY}"); // 0
+        Plugin.Logger.LogInfo($"fresnel power: {__instance.FresnelSettings.FresnelPower}"); // 1
+        Plugin.Logger.LogInfo($"fxaa: {__instance.FXAASettings.Enable}"); // false
+        Plugin.Logger.LogInfo($"scanline contribution: {__instance.ScanlineSettings.Contribution}"); // 0
+        Plugin.Logger.LogInfo($"scanline x: {__instance.ScanlineSettings.ScanlineX}"); // 2
+        Plugin.Logger.LogInfo($"scanline y: {__instance.ScanlineSettings.ScanlineY}"); // 1
+        */
+
+        // Film grain
+        if (Plugin.FilmGrain.Value)
+            __instance.GrainSettings.Size = 0f;
+
+    } // end FMColorPatch
+
+    /// <summary>
+    /// Multiply boss health.
+    /// </summary>
+    [HarmonyPatch(typeof(UI_BossHP), "ConnectWithEntity"), HarmonyPrefix]
+    public static void BossHealthMult(FMFilterGrain __instance, Entity entity)
+    {
+        entity.Health.MaxHealth *= Plugin.BossHealthMult.Value;
+        entity.Health.ScaleMaxHealth *= Plugin.BossHealthMult.Value;
+        entity.Health.CurrentHealth *= Plugin.BossHealthMult.Value;
+
+    } // end BossHealthMult
+
+} // end class Patches
---
+++ Plugin.cs
@@ -0,0 +1,31 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace oinky_mod;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    new public static ManualLogSource Logger { get; private set; }
+    Harmony _harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+
+    public static ConfigEntry<float> BossHealthMult { get; private set; }
+    public static ConfigEntry<bool> FilmGrain { get; private set; }
+
+    private void Awake()
+    {
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loading...");
+
+        BossHealthMult = Config.Bind("General", "BossHealthMult", 0.5f, "Multiplier for boss health. For example, a value of 0.5 means half.");
+        FilmGrain = Config.Bind("General", "FilmGrain", false, "Enable/disable film grain.");
+
+        _harmony.PatchAll(typeof(Patches));
+
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+
+    } // end Awake
+
+} // end class Plugin
